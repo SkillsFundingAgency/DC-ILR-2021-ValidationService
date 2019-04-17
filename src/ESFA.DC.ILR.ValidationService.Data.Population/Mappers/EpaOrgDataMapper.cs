e21468b58facb1aba56316b6335e4c419ca018db
Ashley Beecham <ash@momentoussolutions.co.uk>
2019-04-17 13:57:53 +0100

    73549 - EPA Organisations Mapper implemented

---
+++ src/ESFA.DC.ILR.ValidationService.Data.Population/Mappers/EpaOrgDataMapper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ESFA.DC.ILR.ReferenceDataService.Model.EPAOrganisations;
+using ESFA.DC.ILR.ValidationService.Data.External.EPAOrganisation.Model;
+using ESFA.DC.ILR.ValidationService.Data.Population.Interface;
+
+namespace ESFA.DC.ILR.ValidationService.Data.Population.Mappers
+{
+    public class EpaOrgDataMapper : IEpaOrgDataMapper
+    {
+        public EpaOrgDataMapper()
+        {
+        }
+
+        public IReadOnlyDictionary<string, List<EPAOrganisations>> MapEpaOrganisations(IReadOnlyCollection<EPAOrganisation> epaOrganisations)
+        {
+            return epaOrganisations
+                .GroupBy(e => e.ID)
+                .ToDictionary(
+                k => k.Key,
+                v => v.Select(epa => new EPAOrganisations
+                {
+                    ID = epa.ID,
+                    Standard = epa.Standard,
+                    EffectiveFrom = epa.EffectiveFrom,
+                    EffectiveTo = epa.EffectiveTo
+                }).ToList(), StringComparer.OrdinalIgnoreCase);
+        }
+    }
+}
