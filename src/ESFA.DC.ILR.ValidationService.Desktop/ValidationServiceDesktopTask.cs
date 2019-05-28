ab19bcc091a7de1a9d7db1ad97d9229063a52e9f
Mike Childs <mike@mjcdev.co.uk>
2019-05-28 16:09:31 +0100

    Desktop Task Creation

---
+++ src/ESFA.DC.ILR.ValidationService.Desktop/ValidationServiceDesktopTask.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using ESFA.DC.ILR.Desktop.Interface;
+
+namespace ESFA.DC.ILR.ValidationService.Desktop
+{
+    public class ValidationServiceDesktopTask : IDesktopTask
+    {
+        public Task<IDesktopContext> ExecuteAsync(IDesktopContext desktopContext, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
