361bb7464ba2467c7c331130a076d4831e213a44
Mike Childs <mike@mjcdev.co.uk>
2019-05-29 10:21:43 +0100

    Initial Module Plumbing

---
+++ src/ESFA.DC.ILR.ValidationService.Modules/ValidationServicesModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+using ESFA.DC.ILR.Model.Interface;
+using ESFA.DC.ILR.ValidationService.Interface;
+
+namespace ESFA.DC.ILR.ValidationService.Modules
+{
+    public class ValidationServicesModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterMock<IRuleSetOrchestrationService<IMessage, IValidationError>>();
+        }
+    }
+}
