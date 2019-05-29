34826c37daffaf176456792596af869b2198852c
kelvin.morris <kelvin.morris@eas.co.uk>
2019-05-29 14:37:57 +0100

    #75472 Refactoring PreValidationOrchestrationSfService to extract Actor management and remove and ServiceFabric references from project

--- src/ESFA.DC.ILR.ValidationService.Providers/PreValidationOrchestrationSfService.cs
+++ src/ESFA.DC.ILR.ValidationService.Providers/PreValidationOrchestrationSfService.cs
@@ -1,66 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Fabric;
-using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using ESFA.DC.ILR.Model.Interface;
-using ESFA.DC.ILR.ValidationService.Data.Cache;
 using ESFA.DC.ILR.ValidationService.Data.Interface;
 using ESFA.DC.ILR.ValidationService.Data.Population.Interface;
 using ESFA.DC.ILR.ValidationService.Interface;
 using ESFA.DC.ILR.ValidationService.Interface.Enum;
-using ESFA.DC.ILR.ValidationService.ValidationActor.Interfaces;
-using ESFA.DC.ILR.ValidationService.ValidationActor.Interfaces.Models;
-using ESFA.DC.ILR.ValidationService.ValidationDPActor.Interfaces;
-using ESFA.DC.ILR.ValidationService.ValidationDPActor.Interfaces.Models;
 using ESFA.DC.Logging.Interfaces;
-using ESFA.DC.Serialization.Interfaces;
-using Microsoft.ServiceFabric.Actors;
-using Microsoft.ServiceFabric.Actors.Client;
 
 namespace ESFA.DC.ILR.ValidationService.Providers
 {
     public class PreValidationOrchestrationSfService<U> : IPreValidationOrchestrationService<U>
     {
         private readonly IPopulationService _preValidationPopulationService;
-        private readonly ILearnerPerActorProviderService _learnerPerActorProviderService;
-        private readonly ILearnerDPPerActorProviderService _learnerDPPerActorProviderService;
-        private readonly IJsonSerializationService _jsonSerializationService;
-        private readonly IInternalDataCache _internalDataCache;
-        private readonly IExternalDataCache _externalDataCache;
         private readonly IFileDataCache _fileDataCache;
         private readonly IValidationErrorCache<U> _validationErrorCache;
         private readonly IValidationOutputService _validationOutputService;
         private readonly IRuleSetOrchestrationService<IMessage, U> _ruleSetOrchestrationService;
+        private readonly IValidationExecutionProvider<U> _validationExecutionProvider;
         private readonly ILogger _logger;
 
         public PreValidationOrchestrationSfService(
             IPopulationService preValidationPopulationService,
-            ILearnerPerActorProviderService learnerPerActorProviderService,
-            ILearnerDPPerActorProviderService learnerDPPerActorProviderService,
-            IJsonSerializationService jsonSerializationService,
-            IInternalDataCache internalDataCache,
-            IExternalDataCache externalDataCache,
             IFileDataCache fileDataCache,
             IValidationErrorCache<U> validationErrorCache,
             IValidationOutputService validationOutputService,
             IRuleSetOrchestrationService<IMessage, U> ruleSetOrchestrationService,
+            IValidationExecutionProvider<U> validationExecutionProvider,
             ILogger logger)
         {
             _preValidationPopulationService = preValidationPopulationService;
-            _learnerPerActorProviderService = learnerPerActorProviderService;
-            _learnerDPPerActorProviderService = learnerDPPerActorProviderService;
-            _jsonSerializationService = jsonSerializationService;
-            _internalDataCache = internalDataCache;
-            _externalDataCache = externalDataCache;
             _fileDataCache = fileDataCache;
             _validationErrorCache = validationErrorCache;
             _validationOutputService = validationOutputService;
             _ruleSetOrchestrationService = ruleSetOrchestrationService;
+            _validationExecutionProvider = validationExecutionProvider;
             _logger = logger;
         }
 
@@ -96,7 +72,7 @@ namespace ESFA.DC.ILR.ValidationService.Providers
                     return;
                 }
 
-                await ExecuteValidationActors(validationContext, cancellationToken).ConfigureAwait(false);
+                await _validationExecutionProvider.ExecuteAsync(validationContext, cancellationToken).ConfigureAwait(false);
 
                 _logger.LogDebug(
                     $"Actors results collated {_validationErrorCache.ValidationErrors.Count} validation errors");
@@ -112,180 +88,6 @@ namespace ESFA.DC.ILR.ValidationService.Providers
                 await _validationOutputService.ProcessAsync(validationContext, cancellationToken).ConfigureAwait(false);
                 _logger.LogDebug($"Validation final results persisted in {stopWatch.ElapsedMilliseconds}");
             }
-        }
-
-        private IValidationActor GetValidationActor()
-        {
-            return ActorProxy.Create<IValidationActor>(
-                ActorId.CreateRandom(),
-                new Uri($"{FabricRuntime.GetActivationContext().ApplicationName}/ValidationActorService"));
-        }
-
-        private IValidationDPActor GetValidationDPActor()
-        {
-            return ActorProxy.Create<IValidationDPActor>(
-                ActorId.CreateRandom(),
-                new Uri($"{FabricRuntime.GetActivationContext().ApplicationName}/ValidationDPActorService"));
-        }
-
-        private async Task DestroyValidationActorAsync(IValidationActor validationActor, CancellationToken cancellationToken)
-        {
-            try
-            {
-                ActorId actorId = validationActor.GetActorId();
-
-                IActorService myActorServiceProxy = ActorServiceProxy.Create(
-                    new Uri($"{FabricRuntime.GetActivationContext().ApplicationName}/ValidationActorService"),
-                    actorId);
-
-                await myActorServiceProxy.DeleteActorAsync(actorId, cancellationToken);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError("Problem deleting actor", ex);
-            }
-        }
-
-        private async Task DestroyValidationDPActorAsync(IValidationDPActor validationDPActor, CancellationToken cancellationToken)
-        {
-            try
-            {
-                ActorId actorId = validationDPActor.GetActorId();
-
-                IActorService myActorServiceProxy = ActorServiceProxy.Create(
-                    new Uri($"{FabricRuntime.GetActivationContext().ApplicationName}/ValidationActorService"),
-                    actorId);
-
-                await myActorServiceProxy.DeleteActorAsync(actorId, cancellationToken);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError("Problem deleting actor", ex);
-            }
-        }
-
-        private async Task ExecuteValidationActors(IValidationContext validationContext, CancellationToken cancellationToken)
-        {
-            // Get L/A and split the learners into separate lists
-            IEnumerable<IMessage> learnerMessageShards = await _learnerPerActorProviderService.ProvideAsync();
-            IEnumerable<IMessage> learnerDPMessageShards = await _learnerDPPerActorProviderService.ProvideAsync();
-
-            List<IValidationActor> learnerValidationActors = new List<IValidationActor>();
-            List<IValidationDPActor> learnerDPValidationActors = new List<IValidationDPActor>();
-            List<Task<string>> actorTasks = new List<Task<string>>();
-            List<Task> actorDestroys = new List<Task>();
-
-            Stopwatch stopWatch = new Stopwatch();
-            stopWatch.Start();
-
-            _logger.LogDebug($"Validation will create {learnerMessageShards?.Count() ?? 0} actors");
-            _logger.LogDebug($"DP Validation will create {learnerDPMessageShards?.Count() ?? 0} actors");
-
-            string internalDataCacheAsString =
-                _jsonSerializationService.Serialize(_internalDataCache);
-            _logger.LogDebug($"_internalDataCache {internalDataCacheAsString.Length}");
-            string fileDataCacheAsString =
-                _jsonSerializationService.Serialize(_fileDataCache);
-            _logger.LogDebug($"fileDataCacheAsString {fileDataCacheAsString.Length}");
-            string externalDataCacheAsString =
-                _jsonSerializationService.Serialize(_externalDataCache);
-            _logger.LogDebug($"ExternalDataCache: {externalDataCacheAsString.Length}");
-            string taskListAsString = _jsonSerializationService.Serialize(validationContext.IgnoredRules);
-            _logger.LogDebug($"taskListAsString {taskListAsString.Length}");
-
-            if (learnerMessageShards != null)
-            {
-                foreach (IMessage messageShard in learnerMessageShards)
-                {
-                    _logger.LogDebug($"Validation Shard has {messageShard.Learners.Count} learners");
-
-                    // create actors for each Shard.
-                    IValidationActor actor = GetValidationActor();
-                    learnerValidationActors.Add(actor);
-
-                    // TODO:get reference data per each shard and send it to Actors
-                    string ilrMessageAsString = _jsonSerializationService.Serialize(messageShard);
-
-                    ValidationActorModel validationActorModel = new ValidationActorModel
-                    {
-                        JobId = validationContext.JobId,
-                        Message = ilrMessageAsString,
-                        InternalDataCache = internalDataCacheAsString,
-                        ExternalDataCache = externalDataCacheAsString,
-                        FileDataCache = fileDataCacheAsString,
-                        TaskList = taskListAsString
-                    };
-
-                    actorTasks.Add(actor.Validate(validationActorModel, cancellationToken));
-                }
-            }
-
-            if (learnerDPMessageShards != null)
-            {
-                foreach (IMessage messageShard in learnerDPMessageShards)
-                {
-                    _logger.LogDebug($"DP Validation Shard has {messageShard.LearnerDestinationAndProgressions.Count} learner DP records");
-
-                    // create actors for each Shard.
-                    IValidationDPActor actor = GetValidationDPActor();
-                    learnerDPValidationActors.Add(actor);
-
-                    // TODO:get reference data per each shard and send it to Actors
-                    string ilrMessageAsString = _jsonSerializationService.Serialize(messageShard);
-
-                    ValidationDPActorModel validationActorModel = new ValidationDPActorModel
-                    {
-                        JobId = validationContext.JobId,
-                        Message = ilrMessageAsString,
-                        InternalDataCache = internalDataCacheAsString,
-                        ExternalDataCache = externalDataCacheAsString,
-                        FileDataCache = fileDataCacheAsString,
-                        TaskList = taskListAsString
-                    };
-
-                    actorTasks.Add(actor.Validate(validationActorModel, cancellationToken));
-                }
-            }
-
-            _logger.LogDebug($"Starting {actorTasks.Count} validation actors after {stopWatch.ElapsedMilliseconds}ms prep time");
-            stopWatch.Restart();
-
-            await Task.WhenAll(actorTasks.ToArray()).ConfigureAwait(false);
-
-            cancellationToken.ThrowIfCancellationRequested();
-
-            _logger.LogDebug($"Collating {actorTasks.Count} validation actors after {stopWatch.ElapsedMilliseconds}ms execution time");
-            stopWatch.Restart();
-
-            foreach (Task<string> actorTask in actorTasks)
-            {
-                IEnumerable<U> errors = _jsonSerializationService.Deserialize<IEnumerable<U>>(actorTask.Result);
-
-                foreach (U error in errors)
-                {
-                    _validationErrorCache.Add(error);
-                }
-            }
-
-            _logger.LogDebug($"Destroying {actorTasks.Count} validation actors after {stopWatch.ElapsedMilliseconds}ms collation time");
-
-            foreach (IValidationActor validationActor in learnerValidationActors)
-            {
-                actorDestroys.Add(DestroyValidationActorAsync(validationActor, cancellationToken));
-            }
-
-            foreach (IValidationDPActor validationDPActor in learnerDPValidationActors)
-            {
-                actorDestroys.Add(DestroyValidationDPActorAsync(validationDPActor, cancellationToken));
-            }
-
-            await Task.WhenAll(actorDestroys.ToArray()).ConfigureAwait(false);
-        }
-
-        private bool IsErrorOrFail(U item)
-        {
-            Severity severity = ((IValidationError)item).Severity ?? Severity.Error;
-            return severity == Severity.Error || severity == Severity.Fail;
         }
 
         private bool IsFail(U item)
