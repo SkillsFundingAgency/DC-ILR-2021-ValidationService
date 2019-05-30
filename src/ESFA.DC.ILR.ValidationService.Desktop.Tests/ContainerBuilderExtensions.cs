0338fa42b40a658541c0ee88611f3d177c251401
Mike Childs <mike@mjcdev.co.uk>
2019-05-30 11:07:38 +0100

    Registration of Common Components provided by Desktop Stubbed

---
+++ src/ESFA.DC.ILR.ValidationService.Desktop.Tests/ContainerBuilderExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Autofac;
+using ESFA.DC.DateTimeProvider.Interface;
+
+namespace ESFA.DC.ILR.ValidationService.Desktop.Tests
+{
+    public static class ContainerBuilderExtensions
+    {
+        public static void RegisterCommonServiceStubs(this ContainerBuilder containerBuilder)
+        {
+            containerBuilder.RegisterMock<IDateTimeProvider>();
+            containerBuilder.RegisterMock<ILogger>();
+            //Logger
+            //containerBuilder.RegisterType<FileSystemFileService>().As<IFileService>();
+            //containerBuilder.RegisterType<FileSystemKeyValuePersistenceService>()
+            //    .As<IKeyValuePersistenceService>()
+            //    .As<IStreamableKeyValuePersistenceService>();
+
+            //containerBuilder.RegisterType<DecompressionService>().As<IDecompressionService>();
+
+            //var fileSystemKeyValuePersistenceServiceConfiguration = new FileSystemKeyValuePersistenceServiceConfigStub()
+            //{
+            //    Directory = "Sandbox"
+            //};
+
+            //containerBuilder.RegisterInstance(fileSystemKeyValuePersistenceServiceConfiguration).As<IFileSystemKeyValuePersistenceServiceConfig>();
+
+            //containerBuilder.RegisterType<XmlSerializationService>().As<IXmlSerializationService>();
+            //containerBuilder.RegisterType<JsonSerializationService>().As<IJsonSerializationService>().As<ISerializationService>();
+        }
+
+        public static void RegisterMock<T>(this ContainerBuilder containerBuilder)
+            where T : class
+        {
+            var mock = Mock.Of<T>();
+
+            containerBuilder.RegisterInstance(mock).As<T>();
+        }
+    }
+}
