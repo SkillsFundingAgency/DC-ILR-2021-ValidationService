49741c69824c5b27c624e1d68cee60951aa0dd0b
sharjeel <sharjeel.baloch@eas.co.uk>
2019-07-09 11:47:34 +0000

    minor change

--- src/ESFA.DC.ILR.ValidationService.Rules/LearningDelivery/AchDate/AchDate_09Rule.cs
+++ src/ESFA.DC.ILR.ValidationService.Rules/LearningDelivery/AchDate/AchDate_09Rule.cs
@@ -16,7 +16,7 @@ namespace ESFA.DC.ILR.ValidationService.Rules.LearningDelivery.AchDate
         private readonly int _progTypeApprenticeship = TypeOfLearningProgramme.ApprenticeshipStandard;
         private readonly int _fundModelOtherAdult = TypeOfFunding.OtherAdult;
         private readonly int _fundModelApprencticeMay2017 = TypeOfFunding.ApprenticeshipsFrom1May2017;
-    
+
         public AchDate_09Rule(IValidationErrorHandler validationErrorHandler)
             : base(validationErrorHandler, RuleNameConstants.AchDate_09)
         {
@@ -66,9 +66,8 @@ namespace ESFA.DC.ILR.ValidationService.Rules.LearningDelivery.AchDate
         {
             return !(aimType == _aimType &&
                      (progType == _progTypeTraineeship ||
-                      (progType == _progTypeApprenticeship && fundModel == _fundModelOtherAdult) || 
-                      (progType == _progTypeApprenticeship && fundModel == _fundModelApprencticeMay2017)
-                      ));
+                      (progType == _progTypeApprenticeship && fundModel == _fundModelOtherAdult) ||
+                      (progType == _progTypeApprenticeship && fundModel == _fundModelApprencticeMay2017)));
         }
 
         public IEnumerable<IErrorMessageParameter> BuildErrorMessageParameters(int aimType, DateTime learnStartDate, int? progType, DateTime? achDate)
