89879ab9b9408d7a3a1b20750adee4d8ea8c157d
sharjeel <sharjeel.baloch@eas.co.uk>
2019-07-11 10:12:14 +0000

    83439 - Added new rule PHours_01 and related unit tests

---
+++ src/ESFA.DC.ILR.ValidationService.Rules.Tests/LearningDelivery/PHours/PHours_01RuleTests.cs
@@ -0,0 +1,168 @@
+﻿using ESFA.DC.ILR.Tests.Model;
+using ESFA.DC.ILR.ValidationService.Interface;
+using ESFA.DC.ILR.ValidationService.Rules.Constants;
+using ESFA.DC.ILR.ValidationService.Rules.LearningDelivery.LSDPostcode;
+using ESFA.DC.ILR.ValidationService.Rules.LearningDelivery.PHours;
+using ESFA.DC.ILR.ValidationService.Rules.Tests.Abstract;
+using FluentAssertions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace ESFA.DC.ILR.ValidationService.Rules.Tests.LearningDelivery.PHours
+{
+    public class PHours_01RuleTests : AbstractRuleTests<LSDPostcode_01Rule>
+    {
+        [Fact]
+        public void RuleName()
+        {
+            NewRule().RuleName.Should().Be("PHours_01");
+        }
+
+        [Fact]
+        public void StartDateCondition_Pass_AsDateIsEqual()
+        {
+            var startDate = new DateTime(2019, 8, 1);
+            NewRule().StartDateConditionMet(startDate).Should().BeTrue();
+        }
+
+        [Fact]
+        public void StartDateCondition_Pass_AsDateIsGreater()
+        {
+            var startDate = new DateTime(2019, 12, 1);
+            NewRule().StartDateConditionMet(startDate).Should().BeTrue();
+        }
+
+        [Fact]
+        public void StartDateCondition_Fails_AsDateIsLessThan()
+        {
+            var startDate = new DateTime(2019, 6, 1);
+            NewRule().StartDateConditionMet(startDate).Should().BeFalse();
+        }
+
+        [Fact]
+        public void PlannedHoursConditionMet_Pass()
+        {
+            int? pHours = 40;
+            NewRule().PlannedHoursConditionMet(pHours).Should().BeTrue();
+        }
+
+        [Fact]
+        public void PlannedHoursConditionMet_Fails_AsNull()
+        {
+            int? pHours = null;
+            NewRule().PlannedHoursConditionMet(pHours).Should().BeFalse();
+        }
+
+        [Fact]
+        public void FundModelConditionMet_Pass()
+        {
+            var fundModel = 36;
+            NewRule().FundModelConditionMet(fundModel).Should().BeTrue();
+        }
+
+        [Fact]
+        public void FundModelConditionMet_Fails()
+        {
+            var fundModel = 81;
+            NewRule().FundModelConditionMet(fundModel).Should().BeFalse();
+        }
+
+        [Fact]
+        public void ConditionMet_Pass()
+        {
+            var startDate = new DateTime(2019, 12, 1);
+            int? pHours = 60;
+            int fundModel = 36;
+
+            NewRule().ConditionMet(startDate, pHours, fundModel).Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData("21/06/2019", 200, 36)]
+        [InlineData("01/09/2019", null, 36)]
+        [InlineData("01/09/2019", 300, 81)]
+        public void ConditionMet_Fails(string startingDate, int? pHours, int fundModel)
+        {
+            var startDate = DateTime.Parse(startingDate);
+            NewRule().ConditionMet(startDate, pHours, fundModel).Should().BeFalse();
+        }
+
+        [Fact]
+        public void Validate_Error()
+        {
+            var learnStartDate = new DateTime(2019, 12, 01);
+
+            var learningDeliveries = new List<TestLearningDelivery>()
+            {
+                 new TestLearningDelivery
+                 {
+                     FundModel = 36,
+                     AimType = 1,
+                     PHoursNullable = 200,
+                     LearnStartDate = learnStartDate
+                 }
+            };
+
+            var learner = new TestLearner()
+            {
+                LearnRefNumber = "LearnRefNumber",
+                LearningDeliveries = learningDeliveries
+            };
+
+            using (var validationErrorHandlerMock = BuildValidationErrorHandlerMockForError())
+            {
+                NewRule(validationErrorHandlerMock.Object).Validate(learner);
+            }
+        }
+
+        [Fact]
+        public void Validate_NoError()
+        {
+            var learnStartDate = new DateTime(2019, 12, 01);
+
+            var learningDeliveries = new List<TestLearningDelivery>()
+            {
+                 new TestLearningDelivery
+                 {
+                     FundModel = 81,
+                     AimType = 1,
+                     PHoursNullable = 200,
+                     LearnStartDate = learnStartDate
+                 }
+            };
+
+            var learner = new TestLearner()
+            {
+                LearnRefNumber = "LearnRefNumber",
+                LearningDeliveries = learningDeliveries
+            };
+
+            using (var validationErrorHandlerMock = BuildValidationErrorHandlerMockForNoError())
+            {
+                NewRule(validationErrorHandlerMock.Object).Validate(learner);
+            }
+        }
+
+        [Fact]
+        public void BuildErrorMessageParameters()
+        {
+            var fundModel = 36;
+            var pHours = 200;
+            var validationErrorHandlerMock = new Mock<IValidationErrorHandler>();
+
+            validationErrorHandlerMock.Setup(x => x.BuildErrorMessageParameter(PropertyNameConstants.FundModel, fundModel)).Verifiable();
+            validationErrorHandlerMock.Setup(x => x.BuildErrorMessageParameter(PropertyNameConstants.PHours, pHours)).Verifiable();
+
+            NewRule(validationErrorHandler: validationErrorHandlerMock.Object).BuildErrorMessageParameters(fundModel, pHours);
+
+            validationErrorHandlerMock.Verify();
+        }
+
+        public PHours_01Rule NewRule(IValidationErrorHandler validationErrorHandler = null)
+        {
+            return new PHours_01Rule(validationErrorHandler);
+        }
+    }
+}
---
+++ src/ESFA.DC.ILR.ValidationService.Rules/LearningDelivery/PHours/PHours_01Rule.cs
@@ -0,0 +1,75 @@
+﻿using ESFA.DC.ILR.Model.Interface;
+using ESFA.DC.ILR.ValidationService.Interface;
+using ESFA.DC.ILR.ValidationService.Rules.Abstract;
+using ESFA.DC.ILR.ValidationService.Rules.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ESFA.DC.ILR.ValidationService.Rules.LearningDelivery.PHours
+{
+    public class PHours_01Rule : AbstractRule, IRule<ILearner>
+    {
+        private readonly DateTime _firstAugust2019 = new DateTime(2019, 8, 1);
+
+        private readonly IEnumerable<int> _fundModels = new HashSet<int>()
+        {
+            TypeOfFunding.ApprenticeshipsFrom1May2017
+        };
+
+        public PHours_01Rule(IValidationErrorHandler validationErrorHandler)
+            : base(validationErrorHandler, RuleNameConstants.PHours_01)
+        {
+
+        }
+
+        public void Validate(ILearner objectToValidate)
+        {
+            if (objectToValidate.LearningDeliveries != null)
+            {
+                foreach (var learningDelivery in objectToValidate.LearningDeliveries)
+                {
+                    if (ConditionMet(learningDelivery.LearnStartDate, learningDelivery.PHoursNullable, learningDelivery.FundModel))
+                    {
+                        HandleValidationError(
+                                 objectToValidate.LearnRefNumber,
+                                 learningDelivery.AimSeqNumber,
+                                 BuildErrorMessageParameters(learningDelivery.FundModel, learningDelivery.PHoursNullable));
+                        return;
+                    }
+                }
+            }
+        }
+
+        public bool ConditionMet(DateTime startDate, int? plannedHours, int fundModel)
+        {
+            return StartDateConditionMet(startDate)
+                && PlannedHoursConditionMet(plannedHours)
+                && FundModelConditionMet(fundModel);
+        }
+
+        public bool StartDateConditionMet(DateTime startDate)
+        {
+            return startDate >= _firstAugust2019;
+        }
+
+        public bool PlannedHoursConditionMet(int? plannedHours)
+        {
+            return plannedHours.HasValue;
+        }
+
+        public bool FundModelConditionMet(int fundModel)
+        {
+            return _fundModels.Contains(fundModel);
+        }
+
+        public IEnumerable<IErrorMessageParameter> BuildErrorMessageParameters(int fundModel, int? plannedHours)
+        {
+            return new[]
+            {
+                BuildErrorMessageParameter(PropertyNameConstants.FundModel, fundModel),
+                BuildErrorMessageParameter(PropertyNameConstants.PHours, plannedHours)
+            };
+        }
+    }
+}
