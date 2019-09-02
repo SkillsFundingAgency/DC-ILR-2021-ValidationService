3eaf7ba6f098c8422887b7ab04b944c79c003fd0
sharjeel <sharjeel.baloch@eas.co.uk>
2019-09-02 14:46:42 +0000

    PR comment resolved

--- src/ESFA.DC.ILR.ValidationService.Data/Internal/AcademicYear/Interface/AcademicYearDateExtensions.cs
+++ src/ESFA.DC.ILR.ValidationService.Data/Internal/AcademicYear/Interface/AcademicYearDateExtensions.cs
@@ -13,17 +13,30 @@ namespace ESFA.DC.ILR.ValidationService.Data.Internal.AcademicYear.Interface
         /// </summary>
         private static readonly Dictionary<AcademicYearDates, AcademicYearDateDefinition> _academicYearDateDefinitions = new Dictionary<AcademicYearDates, AcademicYearDateDefinition>()
         {
-            [AcademicYearDates.Commencement] = new AcademicYearDateDefinition(8, 1, GetYear(8)),
-            [AcademicYearDates.PreviousYearEnd] = new AcademicYearDateDefinition(7, 31, GetYear(8)),
-            [AcademicYearDates.August31] = new AcademicYearDateDefinition(8, 31, GetYear(7)),
+            [AcademicYearDates.Commencement] = new AcademicYearDateDefinition(8, 1, x => GetYear(x, 8)),
+            [AcademicYearDates.PreviousYearEnd] = new AcademicYearDateDefinition(7, 31, x => GetYear(x, 8)),
+            [AcademicYearDates.August31] = new AcademicYearDateDefinition(8, 31, x => GetYear(x, 7)),
             [AcademicYearDates.CurrentYearEnd] = new AcademicYearDateDefinition(7, 31, x => x.Month < 8 ? x.Year : x.Year + 1),
             [AcademicYearDates.NextYearCommencement] = new AcademicYearDateDefinition(8, 1, x => x.Month < 8 ? x.Year : x.Year + 1),
         };
 
-        private static Func<DateTime, int> GetYear(int monthToCompare)
+        private static int GetYear(DateTime dateTime, int monthToCompare)
         {
-            int result(DateTime x) => x.Month > monthToCompare ? x.Year : (x.Year > 1 ? x.Year - 1 : x.Year);
-            return result;
+            if (dateTime.Month > monthToCompare)
+            {
+                return dateTime.Year;
+            }
+            else
+            {
+                if (dateTime.Year > 1)
+                {
+                    return dateTime.Year - 1;
+                }
+                else
+                {
+                    return dateTime.Year;
+                }
+            }
         }
 
         /// <summary>
