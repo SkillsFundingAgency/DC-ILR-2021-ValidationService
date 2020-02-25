e4f5432a667ad5bd4dc227e42f58444025b37064
Greg Hunter <GregHunterEsfa@outlook.com>
2020-02-25 11:22:54 +0000

    Added rule template and basic rule name test

---
+++ src/ESFA.DC.ILR.ValidationService.Rules.Tests/LearningDelivery/UKPRN/UKPRN_20RuleTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using ESFA.DC.ILR.Tests.Model;
+using ESFA.DC.ILR.ValidationService.Data.External.FCS.Interface;
+using ESFA.DC.ILR.ValidationService.Data.External.FCS.Model;
+using ESFA.DC.ILR.ValidationService.Data.File.FileData.Interface;
+using ESFA.DC.ILR.ValidationService.Interface;
+using ESFA.DC.ILR.ValidationService.Rules.Constants;
+using ESFA.DC.ILR.ValidationService.Rules.LearningDelivery.UKPRN;
+using ESFA.DC.ILR.ValidationService.Rules.Tests.Abstract;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace ESFA.DC.ILR.ValidationService.Rules.Tests.LearningDelivery.UKPRN
+{
+    public class UKPRN_20RuleTests : AbstractRuleTests<UKPRN_20Rule>
+    {
+        [Fact]
+        public void RuleName()
+        {
+            NewRule().RuleName.Should().Be("UKPRN_20");
+        }
+
+        private UKPRN_20Rule NewRule(
+            IFileDataService fileDataService = null,
+            IFCSDataService fcsDataService = null,
+            IValidationErrorHandler validationErrorHandler = null)
+        {
+            return new UKPRN_20Rule(fileDataService, fcsDataService, validationErrorHandler);
+        }
+    }
+}
---
+++ src/ESFA.DC.ILR.ValidationService.Rules/LearningDelivery/UKPRN/UKPRN_20Rule.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ESFA.DC.ILR.Model.Interface;
+using ESFA.DC.ILR.ValidationService.Data.External.FCS.Interface;
+using ESFA.DC.ILR.ValidationService.Data.File.FileData.Interface;
+using ESFA.DC.ILR.ValidationService.Interface;
+using ESFA.DC.ILR.ValidationService.Rules.Abstract;
+using ESFA.DC.ILR.ValidationService.Rules.Constants;
+
+namespace ESFA.DC.ILR.ValidationService.Rules.LearningDelivery.UKPRN
+{
+    public class UKPRN_20Rule : AbstractRule, IRule<ILearner>
+    {
+        private readonly IFileDataService _fileDataService;
+        private readonly IFCSDataService _fcsDataService;
+
+        public UKPRN_20Rule(
+            IFileDataService fileDataService,
+            IFCSDataService fcsDataService,
+            IValidationErrorHandler validationErrorHandler)
+            : base(validationErrorHandler, RuleNameConstants.UKPRN_20)
+        {
+            _fileDataService = fileDataService;
+            _fcsDataService = fcsDataService;
+        }
+
+        public UKPRN_20Rule()
+           : base(null, null)
+        {
+        }
+
+        public void Validate(ILearner objectToValidate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
