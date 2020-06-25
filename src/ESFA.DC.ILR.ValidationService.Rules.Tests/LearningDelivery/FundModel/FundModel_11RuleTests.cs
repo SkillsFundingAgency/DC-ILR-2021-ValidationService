6d6028b50fe9d6a433c1525910612c69f4a23c93
Cory-Wynn <CoryWynn1993@gmail.com>
2020-06-25 10:35:41 +0000

    101764 FundModel_11

---
+++ src/ESFA.DC.ILR.ValidationService.Rules.Tests/LearningDelivery/FundModel/FundModel_11RuleTests.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Generic;
+using ESFA.DC.ILR.Tests.Model;
+using ESFA.DC.ILR.ValidationService.Interface;
+using ESFA.DC.ILR.ValidationService.Rules.LearningDelivery.FundModel;
+using ESFA.DC.ILR.ValidationService.Rules.Tests.Abstract;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace ESFA.DC.ILR.ValidationService.Rules.Tests.LearningDelivery.FundModel
+{
+    public class FundModel_11RuleTests : AbstractRuleTests<FundModel_11Rule>
+    {
+        [Fact]
+        public void RuleName()
+        {
+            NewRule().RuleName.Should().Be("FundModel_11");
+        }
+
+        [Fact]
+        public void ConditionMet_True()
+        {
+            NewRule().ConditionMet(36, 31).Should().BeTrue();
+        }
+
+        [Fact]
+        public void ConditionMet_False_FundModel()
+        {
+            NewRule().ConditionMet(25, 31).Should().BeFalse();
+        }
+
+        [Fact]
+        public void ConditionMet_False_ProgType()
+        {
+            NewRule().ConditionMet(36, 30).Should().BeFalse();
+        }
+
+        [Fact]
+        public void Validate_Error()
+        {
+            var learner = new TestLearner()
+            {
+                LearningDeliveries = new List<TestLearningDelivery>()
+                {
+                    new TestLearningDelivery()
+                    {
+                        FundModel = 36,
+                        ProgTypeNullable = 31
+                    }
+                }
+            };
+
+            using (var validationErrorHandlerMock = BuildValidationErrorHandlerMockForError())
+            {
+                NewRule(validationErrorHandlerMock.Object).Validate(learner);
+            }
+        }
+
+        [Fact]
+        public void Validate_NoErrors()
+        {
+            var learner = new TestLearner()
+            {
+                LearningDeliveries = new List<TestLearningDelivery>()
+                {
+                    new TestLearningDelivery()
+                    {
+                        FundModel = 25,
+                        ProgTypeNullable = 31
+                    }
+                }
+            };
+
+            using (var validationErrorHandlerMock = BuildValidationErrorHandlerMockForNoError())
+            {
+                NewRule(validationErrorHandlerMock.Object).Validate(learner);
+            }
+        }
+
+        [Fact]
+        public void BuildErrorMessageParameters()
+        {
+            var fundModel = 1;
+            var progType = 31;
+
+            var validationErrorHandlerMock = new Mock<IValidationErrorHandler>();
+
+            validationErrorHandlerMock.Setup(veh => veh.BuildErrorMessageParameter("FundModel", fundModel)).Verifiable();
+            validationErrorHandlerMock.Setup(veh => veh.BuildErrorMessageParameter("ProgType", progType)).Verifiable();
+
+            NewRule(validationErrorHandlerMock.Object).BuildErrorMessageParameters(fundModel, progType);
+
+            validationErrorHandlerMock.Verify();
+        }
+
+        private FundModel_11Rule NewRule(IValidationErrorHandler validationErrorHandler = null)
+        {
+            return new FundModel_11Rule(validationErrorHandler);
+        }
+    }
+}
