c25b27453c55a6e9851d97335138b359d5aa618f
Sam Precious <sam.precious@nimbusdti.co.uk>
2020-08-24 12:10:58 +0000

    Added new rule LearnStartDate_18

---
+++ src/ESFA.DC.ILR.ValidationService.Rules/LearningDelivery/LearnStartDate/LearnStartDate_18Rule.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using ESFA.DC.ILR.Model.Interface;
+using ESFA.DC.ILR.ValidationService.Data.External.LARS.Interface;
+using ESFA.DC.ILR.ValidationService.Interface;
+using ESFA.DC.ILR.ValidationService.Rules.Abstract;
+using ESFA.DC.ILR.ValidationService.Rules.Constants;
+using ESFA.DC.ILR.ValidationService.Rules.Query.Interface;
+
+namespace ESFA.DC.ILR.ValidationService.Rules.LearningDelivery.LearnStartDate
+{
+    public class LearnStartDate_18Rule : AbstractRule, IRule<ILearner>
+    {
+        private readonly ILARSDataService _larsDataService;
+        private readonly ILearningDeliveryFAMQueryService _learningDeliveryFAMQueryService;
+
+        public LearnStartDate_18Rule(
+            ILARSDataService larsDataService,
+            ILearningDeliveryFAMQueryService learningDeliveryFAMQueryService,
+            IValidationErrorHandler validationErrorHandler)
+            : base(validationErrorHandler, RuleNameConstants.LearnStartDate_18)
+        {
+            _larsDataService = larsDataService;
+            _learningDeliveryFAMQueryService = learningDeliveryFAMQueryService;
+        }
+
+        public void Validate(ILearner learner)
+        {
+            if (learner.LearningDeliveries == null)
+            {
+                return;
+            }
+
+            foreach (var learningDelivery in learner.LearningDeliveries)
+            {
+                if (ConditionMet(learningDelivery))
+                {
+                    HandleValidationError(learner.LearnRefNumber, learningDelivery.AimSeqNumber, BuildMessageParametersFor(learningDelivery));
+                }
+            }
+        }
+
+        public bool ConditionMet(ILearningDelivery learningDelivery)
+        {
+            return !_learningDeliveryFAMQueryService.HasLearningDeliveryFAMType(learningDelivery.LearningDeliveryFAMs, LearningDeliveryFAMTypeConstants.RES)
+                && LARSConditionMet(learningDelivery.StdCodeNullable.Value, learningDelivery.LearnStartDate);
+        }
+
+        public bool LARSConditionMet(int stdCode, DateTime learnStartDate)
+        {
+            var larsStandard = _larsDataService.GetStandardFor(stdCode);
+
+            if (larsStandard != null)
+            {
+                return larsStandard.LastDateStarts.HasValue && learnStartDate > larsStandard.LastDateStarts;
+            }
+
+            return false;
+        }
+
+        public IEnumerable<IErrorMessageParameter> BuildMessageParametersFor(ILearningDelivery thisDelivery)
+        {
+            return new[]
+            {
+                BuildErrorMessageParameter(PropertyNameConstants.AimType, thisDelivery.AimType),
+                BuildErrorMessageParameter(PropertyNameConstants.LearnStartDate, thisDelivery.LearnStartDate),
+                BuildErrorMessageParameter(PropertyNameConstants.ProgType, thisDelivery.ProgTypeNullable),
+                BuildErrorMessageParameter(PropertyNameConstants.StdCode, thisDelivery.StdCodeNullable)
+            };
+        }
+    }
+}
