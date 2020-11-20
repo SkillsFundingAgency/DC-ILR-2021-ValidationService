34dfc2976a55abbc76ba8e585bca61eb3bd1ced0
Salman Baig <salman.sfa@outlook.com>
2020-11-20 10:03:14 +0000

    R126 Val Rule

---
+++ src/ESFA.DC.ILR.ValidationService.Rules/LearningDelivery/AimType/AimType_126Rule.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ESFA.DC.ILR.Model.Interface;
+using ESFA.DC.ILR.ValidationService.Interface;
+using ESFA.DC.ILR.ValidationService.Rules.Abstract;
+using ESFA.DC.ILR.ValidationService.Rules.Constants;
+
+namespace ESFA.DC.ILR.ValidationService.Rules.LearningDelivery.AimType
+{
+    public class AimType_126Rule : AbstractRule, IRule<ILearner>
+    {
+        public AimType_126Rule(IValidationErrorHandler validationErrorHandler)
+            : base(validationErrorHandler, RuleNameConstants.AimType_09)
+        {
+        }
+
+        public void Validate(ILearner objectToValidate)
+        {
+            foreach (var learningDelivery in objectToValidate.LearningDeliveries)
+            {
+                if (ConditionMet(learningDelivery.LearnAimRef, learningDelivery.AimType))
+                {
+                    HandleValidationError(objectToValidate.LearnRefNumber, learningDelivery.AimSeqNumber, BuildErrorMessageParameters(learningDelivery.LearnAimRef, learningDelivery.AimType));
+                }
+            }
+        }
+
+        public bool ConditionMet(string learnAimRef, int aimType)
+        {
+            return learnAimRef != null && learnAimRef.StartsWith(ValidationConstants.ZTPR, StringComparison.InvariantCultureIgnoreCase)
+                                       && aimType != AimTypes.CoreAim16To19ExcludingApprenticeships;
+        }
+
+        public IEnumerable<IErrorMessageParameter> BuildErrorMessageParameters(string learnAimRef, int aimType)
+        {
+            return new[]
+            {
+                BuildErrorMessageParameter(PropertyNameConstants.LearnAimRef, learnAimRef),
+                BuildErrorMessageParameter(PropertyNameConstants.AimType, aimType)
+            };
+        }
+    }
+}
